d4bc6966b4468092b1bd8c05043549ddbf75c905
Luis Maya Aranda <mayafluis@gmail.com>
2022-02-16 17:03:10 -0800

    Adding clear command

---
+++ slash/clear.js
@@ -0,0 +1,45 @@
+const { Constants } = require("discord.js");
+
+exports.run = async (client, interaction) => {
+  try {
+    const { member, options, channel } = interaction;
+    const num = options.getNumber("num");
+    // 101
+    let remaining = num;
+    while (remaining > 0) {
+      const limit = remaining > 100 ? 100 : remaining;
+      remaining = remaining > 100 ? remaining - 100 : 0;
+
+      await channel.bulkDelete(limit);
+    }
+    return await interaction.reply({
+      content: `cleared ${num} message(s)`,
+      ephemeral: true,
+    });
+  } catch (e) {
+    console.log(e);
+    return await interaction.reply(
+      `There was a problem with your request.\n\`\`\`${e.message}\`\`\``
+    );
+  }
+};
+
+exports.commandData = {
+  name: "clear",
+  description: "Clear [n] messages from text channel.",
+  options: [
+    {
+      name: "num",
+      description: "Number of messages to clear.",
+      required: true,
+      type: Constants.ApplicationCommandOptionTypes.NUMBER,
+    },
+  ],
+  defaultPermission: true,
+};
+
+// Set this to false if you want it to be global.
+exports.conf = {
+  permLevel: "Administrator",
+  guildOnly: true,
+};
