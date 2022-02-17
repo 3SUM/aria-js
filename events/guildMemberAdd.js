dd19e84ba439d6ff1064eb0a58ff53cfc5dde36a
Luis Maya Aranda <mayafluis@gmail.com>
2022-02-16 22:14:24 -0800

    Further cleanup of memberAdd

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,10 +1,7 @@
-const { getSettings } = require("../modules/functions.js");
-// This event executes when a new member joins a server. Let's welcome them!
-
 module.exports = (client, member) => {
   // Load the guild's settings
   const guild = member.guild;
-	const userRole = guild.roles.cache.find(role => role.name === "User");
-	member.roles.add(userRole);
-  await member.setNickname('0xFFFF');
+  const userRole = guild.roles.cache.find((role) => role.name === "User");
+  member.roles.add(userRole);
+  await member.setNickname("0xFFFF");
 };
